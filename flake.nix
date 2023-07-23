9c218efb1f1e2b062b315814b4cce3fce1172560
xenolf <xenolf@users.noreply.github.com>
2023-07-23 16:20:55 +0200

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "My fabolous config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    # Home manager
+    home-manager = { url = "github:nix-community/home-manager/release-23.05"; inputs.nixpkgs.follows = "nixpkgs"; };
+    # nix utils
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, home-manager, rust-overlay, utils, ... }@inputs:
+  let 
+    pkgsForSystem = system: import nixpkgs {
+      overlays = [
+      ];
+      inherit system;
+      config.allowUnfree = true;
+    };
+
+    mkHomeConfiguration = args: home-manager.lib.homeManagerConfiguration (rec {
+      pkgs = pkgsForSystem (args.system or "x86_64-linux");
+    } // { inherit (args) extraSpecialArgs modules; });
+  in utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin"] (system: rec {
+    legacyPackages = pkgsForSystem system;
+  }) // {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      your-hostname = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        # > Our main nixos configuration file <
+        modules = [ ./nixos/configuration.nix ];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "root@vmpod" = mkHomeConfiguration {
+        modules = [ ./home-manager/vmpod-home.nix ];
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+      };
+    };
+  };
+}
